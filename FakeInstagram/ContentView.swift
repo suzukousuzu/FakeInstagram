00a3776a1979b9d31b0e29f551860c74b0057db8
Kota Suzuki <addressuzuki@gmail.com>
2024-03-13 20:08:39 +0900

    Initial Commit

---
+++ FakeInstagram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FakeInstagram
+//
+//  Created by 鈴木航太 on 2024/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FakeInstagram/FakeInstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeInstagramApp.swift
+//  FakeInstagram
+//
+//  Created by 鈴木航太 on 2024/03/13.
+//
+
+import SwiftUI
+
+@main
+struct FakeInstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
